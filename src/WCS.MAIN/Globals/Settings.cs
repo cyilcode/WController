a70835efe3a214c93f1072553484d6c487b2f967
Cem YILMAZ <cmylmzbm@outlook.com>
2016-09-09 23:46:49 +0300

    Initial implementation of Settings reader

---
+++ src/WCS.MAIN/Globals/Settings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WCS.MAIN.Globals
+{
+    public class Settings
+    {
+        /*Git connection test. Seems like my connection here is a bit shaky.*/
+        private const string OSX_CONF_FILE = "";
+        private const string WIN_CONF_FILE = "";
+        private const string LIN_CONF_FILE = "";
+        public Settings(OS platform)
+        {
+
+        }
+    }
+}
